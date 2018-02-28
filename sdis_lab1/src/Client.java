fccc418cdbb146e3bcab92eaf6b83266bdc5de44
FilipeMRibeiro <filipeml.ribeiro93@gmail.com>
2018-02-28 14:38:58 +0000

    added lab1&2 guides

--- sdis_lab1/src/Client.java
+++ sdis_lab1/src/Client.java
@@ -8,13 +8,16 @@ public class Client {
 	private static String host_name;
 	private static int port;
 	private static String request_type, plate_number, owner_name;
-	
+
 	public static void main(String[] args) throws IOException {
 		//Check args
 		if(args.length != 4 && args.length != 5) {
 			System.out.println("Error. Wrong number of arguments.");
 			System.out.println("Please try as follows: java Client <host_name> <port_number> [register <plate_number> <owner_name> | lookup <plate_number>]");;
 			return;
+		}
+		else if{
+			System.out.println(esta);
 		}
 		else if(args[2].compareTo("register") == 0 && args.length != 5) {
 			System.out.println("Error. Wrong number of arguments for register command.");
@@ -37,24 +40,24 @@ public class Client {
 				request = new String(request_type + "&" + plate_number + "&" + owner_name);
 			}
 			else request = new String(request_type + "&" + plate_number);
-			
+
 			//Open Socket
 			DatagramSocket socket = new DatagramSocket();
 			System.out.println("Socket was open.");
-			
+
 			//Send request
 			byte[] buffer = request.getBytes();
 			InetAddress address = InetAddress.getLocalHost();
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
 			socket.send(packet);
 			System.out.println("Sent: " + request);
-			
+
 			//Receive response
 			packet = new DatagramPacket(buffer, buffer.length);
 			socket.receive(packet);
 			String response = new String(packet.getData(), 0, packet.getLength());
 			System.out.println("Received: " + response);
-			
+
 			//Close socket
 			socket.close();
 		}
